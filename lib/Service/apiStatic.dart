4bdd93adc63e9e31f7b51d7f5e03e64928a74a4c
002_galang <galang.wijaya@undiksha.ac.id>
2021-11-28 23:15:53 +0800

    searching

--- lib/Service/apiStatic.dart
+++ lib/Service/apiStatic.dart
@@ -6,8 +6,8 @@ import 'package:flutter_coba1/Model/errMSG.dart';
 
 class ApiStatic {
   late int id_phone = 0;
-  static final _token = "9|rL6uPorvmFk4wdppElaGhNooIlOawfiySwEiJMQe";
-  static final host = 'http://192.168.1.4/API2021/public/api/';
+  static final _token = "10|uBozEtMKxFWFwPGOSroTSnUocnRAcGzEWT3naxzr";
+  static final host = 'http://192.168.1.9/API2021/public/api/';
   static getHost() {
     return host;
   }
@@ -34,9 +34,34 @@ class ApiStatic {
     //     '{"data":[{"IDphone":3,"nama":"X3 GT","kdphone":"2021","merk":"Xiaomi","asalhp":null,"foto":null,"created_at":"2021-09-25T13:53:19.000000Z","updated_at":"2021-09-25T13:53:19.000000Z"}]}';
     try {
       final response = await http.get(
-          Uri.parse('http://192.168.1.4/API2021/public/api/phone'),
+          Uri.parse('http://192.168.1.9/API2021/public/api/phone'),
           headers: {
             'Authorization': 'Bearer' + _token,
+          });
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        final parsed = json['data'].cast<Map<String, dynamic>>();
+        return parsed.map<Ponsel>((json) => Ponsel.fromJson(json)).toList();
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
+  }
+
+  static Future<List<Ponsel>> getPonselFilter(
+      int pageKey, String _s, String _selectedChoice) async {
+    try {
+      final response = await http.get(
+          Uri.parse("http://192.168.1.9/API2021/public/api/phone?page=" +
+              pageKey.toString() +
+              "&s=" +
+              _s +
+              "&publish=" +
+              _selectedChoice),
+          headers: {
+            'Authorization': 'Bearer ' + _token,
           });
       if (response.statusCode == 200) {
         var json = jsonDecode(response.body);
@@ -55,7 +80,7 @@ class ApiStatic {
     //     '{"data":[{"IDphone":3,"nama":"X3 GT","kdphone":"2021","merk":"Xiaomi","asalhp":null,"foto":null,"created_at":"2021-09-25T13:53:19.000000Z","updated_at":"2021-09-25T13:53:19.000000Z"}]}';
     try {
       final response = await http.get(
-          Uri.parse('http://192.168.1.4/API2021/public/api/brand'),
+          Uri.parse('http://192.168.1.9/API2021/public/api/brand'),
           headers: {
             'Authorization': 'Bearer' + _token,
           });
@@ -73,10 +98,10 @@ class ApiStatic {
 
   static Future<ErrorMSG> savePonsel(id, ponsel, filepath) async {
     try {
-      var url = Uri.parse('http://192.168.1.4/API2021/public/api/phone');
+      var url = Uri.parse('http://192.168.1.9/API2021/public/api/phone');
       if (id != 0) {
         url = Uri.parse(
-            'http://192.168.1.4/API2021/public/api/phone' + id.toString());
+            'http://192.168.1.9/API2021/public/api/phone' + id.toString());
       }
 
       var request = http.MultipartRequest('POST', url);
@@ -108,11 +133,11 @@ class ApiStatic {
     }
   }
 
-  static Future<ErrorMSG> deletePhone(id) async {
+  static Future<ErrorMSG> deletePhone(id_phone) async {
     try {
       final response = await http.delete(
-          Uri.parse(
-              'http://192.168.1.4/API2021/public/api/phone' + id.toString()),
+          Uri.parse('http://192.168.1.9/API2021/public/api/phone' +
+              id_phone.toString()),
           headers: {
             'Authorization': 'Bearer' + _token,
           });
--- lib/UI/ponsellist.dart
+++ lib/UI/ponsellist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:flutter_coba1/Model/errMSG.dart';
 import 'package:flutter_coba1/Model/ponsel.dart';
 import 'package:flutter_coba1/Service/apiStatic.dart';
@@ -15,17 +16,49 @@ class ponselpage extends StatefulWidget {
 
 class _ponselpageState extends State<ponselpage> {
   late ErrorMSG response;
-  void deletePonsel(id) async {
-    response = await ApiStatic.deletePhone(id);
+  final PagingController<int, Ponsel> _pagingController =
+      PagingController(firstPageKey: 0);
+  late TextEditingController _s;
+  final GlobalKey<ScaffoldState> _scaffoldkey = new GlobalKey<ScaffoldState>();
+  late String _publish = "Y";
+  int _pageSize = 3;
+
+  void deletePonsel(id_phone) async {
+    response = await ApiStatic.deletePhone(id_phone);
     final snackBar = SnackBar(
       content: Text(response.message),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
+  Future<void> _fetchPage(int pageKey, _s, _publish) async {
+    try {
+      final newItems = await ApiStatic.getPonselFilter(pageKey, _s, _publish);
+      final isLastPage = newItems.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + 1;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
+
+  @override
+  void initState() {
+    _s = TextEditingController();
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey, _s.text, _publish);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldkey,
       appBar: AppBar(
         title: Text("Phones"),
       ),
@@ -49,49 +82,48 @@ class _ponselpageState extends State<ponselpage> {
                       ))));
         },
       ),
-      body: FutureBuilder<List<Ponsel>>(
-        future: ApiStatic.getPonsel(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            List<Ponsel> listponsel = snapshot.data!;
-            return Container(
-              padding: EdgeInsets.all(5),
-              child: ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (BuildContext context, int index) => Column(
-                  children: [
-                    InkWell(
+      body: SingleChildScrollView(
+        child: Stack(children: [
+          Container(
+            padding: EdgeInsets.only(top: 80),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: RefreshIndicator(
+              onRefresh: () => Future.sync(() => _pagingController.refresh()),
+              child: PagedListView<int, Ponsel>(
+                pagingController: _pagingController,
+                builderDelegate: PagedChildBuilderDelegate<Ponsel>(
+                  itemBuilder: (context, item, index) => Container(
+                    child: InkWell(
                       onTap: () {
                         Navigator.of(context).push(new MaterialPageRoute(
                             builder: (BuildContext context) => detailponsel(
-                                  ponsel: listponsel[index],
+                                  ponsel: item,
                                 )));
                       },
                       child: Container(
+                        height: 150,
                         padding: EdgeInsets.all(5),
                         margin: EdgeInsets.only(top: 10),
                         width: double.infinity,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(3),
                             color: Colors.white,
-                            border: Border.all(
-                                width: 1, color: Colors.lightGreenAccent)),
+                            border: Border.all(width: 1)),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             // Image.network(
-                            //   listponsel[index].foto,
-                            //   width: 20,
+                            //   ApiStatic.host + '/' + item.foto,
+                            //   width: 30,
                             // ),
                             Padding(
-                              padding: const EdgeInsets.only(left: 4, right: 4),
+                              padding: const EdgeInsets.only(left: 5, right: 5),
                               child: Column(
                                 children: [
-                                  Text(listponsel[index].nama),
+                                  Text(item.nama),
                                   Text(
-                                    listponsel[index].kd_phone,
+                                    item.kd_phone,
                                     style: TextStyle(fontSize: 8),
                                   ),
                                 ],
@@ -106,13 +138,14 @@ class _ponselpageState extends State<ponselpage> {
                                           new MaterialPageRoute(
                                               builder: (BuildContext context) =>
                                                   InputPonsel(
-                                                    ponsel: listponsel[index],
+                                                    ponsel: item,
                                                   )));
                                     },
                                     child: Icon(Icons.edit)),
                                 GestureDetector(
                                     onTap: () {
-                                      deletePonsel(listponsel[index].id_phone);
+                                      deletePonsel(item.id_phone);
+                                      _pagingController.refresh();
                                     },
                                     child: Icon(Icons.delete)),
                               ],
@@ -120,13 +153,102 @@ class _ponselpageState extends State<ponselpage> {
                           ],
                         ),
                       ),
-                    )
-                  ],
+                    ),
+                  ),
                 ),
               ),
-            );
-          }
-        },
+            ),
+          ),
+          Container(
+            height: 100,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30))),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () => _scaffoldkey.currentState!.openDrawer(),
+                    icon: Icon(
+                      Icons.menu,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    "Data Ponsel",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  PopupMenuButton(
+                    icon: Icon(
+                      Icons.filter_list,
+                      color: Colors.black,
+                    ),
+                    initialValue: _publish,
+                    onSelected: (String result) {
+                      setState(() {
+                        _publish = result;
+                        _pagingController.refresh();
+                      });
+                    },
+                    itemBuilder: (BuildContext context) =>
+                        <PopupMenuItem<String>>[
+                      new PopupMenuItem<String>(
+                          child: const Text('Aktif'), value: 'Y'),
+                      new PopupMenuItem<String>(
+                          child: const Text('Non Aktif'), value: 'N'),
+                      new PopupMenuItem<String>(
+                          child: const Text('Semua'), value: 'All'),
+                      new PopupMenuItem<String>(
+                          child: const Text('Deleted'), value: 'del'),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          //contaoner untuk search
+          Container(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 70),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    child: TextField(
+                      controller: _s,
+                      onSubmitted: (_s) {
+                        _pagingController.refresh();
+                      },
+                      cursorColor: Theme.of(context).primaryColor,
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                      decoration: InputDecoration(
+                          hintText: "Masukkan Nama Ponsel",
+                          hintStyle:
+                              TextStyle(color: Colors.black38, fontSize: 16),
+                          prefixIcon: Material(
+                            elevation: 0.0,
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            child: Icon(Icons.search),
+                          ),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 25, vertical: 13)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
